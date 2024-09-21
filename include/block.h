793095adafcaf33f944be591d6dcb905b86b63f3
jujudave <jujudave1105@outlook.fr>
2024-09-21 23:04:03 +0200

    Begin of block and entity logistic

--- include/block.h
+++ include/block.h
@@ -1,6 +1,12 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
+typedef struct {
+    char symbol;
+    int posX;
+    int posY;
 
+    void (*action)(enum Action);
+} Block;
 
 #endif
--- include/entity.h
+++ include/entity.h
@@ -1,4 +1,12 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+typedef struct  {
+    char symbol;
+    int posX;
+    int posY;
+
+    void (*action)();
+} Entity;
+
 #endif
