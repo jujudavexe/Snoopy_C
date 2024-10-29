ff117712418e74549890bf2fe46e9d8b0846d443
jujudave <jujudave1105@outlook.fr>
2024-10-29 23:10:20 +0100

    Update of entity logic

--- include/entity.h
+++ include/entity.h
@@ -1,12 +1,19 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+enum EntityAction {
+    PASS,
+    PUSH,
+    BREACK
+};
+
 typedef struct  {
     char symbol;
     int posX;
     int posY;
+    int canInteract;
 
-    void (*action)();
+    void (*action)(); //Recevra l'action de l'entité et invoquera action de block si y'en a un sur la case actionné
 } Entity;
 
 #endif
